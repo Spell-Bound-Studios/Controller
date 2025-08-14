c29d04880ead80601f08572293bd2b75ff2fb1e2
Judsin1349 <judsin1349@gmail.com>
2025-08-13 22:43:33 -0500

    updated player controller and camera

---
+++ Runtime/Configuration/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+using Helper = SpellBound.Controller.Configuration.ControllerHelper;
+
+namespace SpellBound.Controller.Configuration {
+    public class CameraController : MonoBehaviour {
+        [SerializeField] private CinemachineCameraManagerBase cameraRig;
+        [SerializeField] private Helper.CameraCouplingMode playerRotationMode;
+        private CinemachineBrain _brain;
+
+        private void Awake() {
+            if (!_brain && Camera.main) 
+                Camera.main.TryGetComponent(out _brain);
+        }
+        
+        private void Start() {
+            CameraSetup();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+        private void CameraSetup() {
+            cameraRig = _brain.ActiveVirtualCamera as CinemachineCameraManagerBase;
+
+            if (cameraRig == null) {
+                Debug.LogError("Camera rig is null and doesn't appear to be in scene.");
+                return;
+            }
+
+            cameraRig.DefaultTarget.Target.TrackingTarget = transform;
+        }
+    }
+}
---
+++ Runtime/Configuration/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpellBound.Controller.Configuration {
+    public class PlayerController : MonoBehaviour {
+        private InputActions _inputActions;
+        private Vector2 _moveInput;
+        
+        private void Awake() {
+            _inputActions = new InputActions();
+        }
+
+        private void OnEnable() {
+            _inputActions.Enable();
+            
+            // Movement (WASD)
+            _inputActions.PlayerInput.Movement.performed += OnMovePerformed;
+            _inputActions.PlayerInput.Movement.canceled += OnMoveCanceled;
+            
+            // Jump (space bar)
+            _inputActions.PlayerInput.Jump.performed += _ => OnJumpInput();
+        }
+        
+        private void OnDisable() {
+            _inputActions.Disable();
+        }
+        
+        private void OnMovePerformed(InputAction.CallbackContext ctx) => _moveInput = ctx.ReadValue<Vector2>();
+        private void OnMoveCanceled(InputAction.CallbackContext ctx) => _moveInput = Vector2.zero;
+        
+        private void OnJumpInput() {
+            Debug.Log("Jumping.");
+        }
+    }
+}
