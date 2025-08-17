b01ad090a77e82da010336c2e865e0977c71fc77
Judsin1349 <judsin1349@gmail.com>
2025-08-16 19:17:04 -0500

    maybe finally got the freelook camera down

---
+++ Runtime/Configuration/CameraAnchorFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SpellBound.Controller.Configuration {
+    public class CameraAnchorFollower : MonoBehaviour {
+        [SerializeField] private Transform playerRoot;
+        [SerializeField] private Vector3 offset = new(-0.2f, 1.5f, 0f);
+
+        private void Awake() {
+            if (!playerRoot)
+                Debug.LogError("PlayerRoot is not set. Please drag it in via inspector.", this);
+        }
+        
+        private void LateUpdate() {
+            transform.position = playerRoot.position + offset;
+        }
+    }
+}
--- Runtime/Configuration/CameraController.cs
+++ Runtime/Configuration/CameraController.cs
@@ -7,13 +7,19 @@ namespace SpellBound.Controller.Configuration {
     /// Interface for how the player inputs drive its members.
     /// </summary>
     public class CameraController : MonoBehaviour {
+        // How high you can move your camera.
         [SerializeField, Range(0f, 90f)] private float upperVerticalLimit = 89f;
+        // How low you can move your camera.
         [SerializeField, Range(0f, 90f)] private float lowerVerticalLimit = 89f;
+        // How fast you can move your camera.
         [SerializeField] private float cameraSpeed = 25f;
+        // Optional lerp applied to input.
         [SerializeField] private bool smoothCameraRotation;
+        // Multiplies the time.deltaTime by this factor.
         [SerializeField, Range(1f, 50f)] private float cameraSmoothingFactor = 25f;
         [SerializeField, Min(0.1f)] private float minZoomDistance = 1f;
         [SerializeField, Min(1f)] private float maxZoomDistance = 8f;
+        // Controls how fast you zoom in and out.
         [SerializeField] private float zoomIncrement = .2f;
         
         [SerializeField] private bool cursorLockOnStart = true;
@@ -23,7 +29,9 @@ namespace SpellBound.Controller.Configuration {
         
         private CinemachineBrain _brain;
         private Transform _tr;
+        // Cached local rotation value X.
         private float _currentXAngle;
+        // Cached local rotation value Y.
         private float _currentYAngle;
 
         [SerializeField] private Transform cameraPivot;
@@ -42,22 +50,24 @@ namespace SpellBound.Controller.Configuration {
         }
 
         private void OnEnable() {
-            input.OnMouseWheelInput += ZoomCamera;
+            CameraSetup();
+
+            if (cameraRig)
+                input.OnMouseWheelInput += ZoomCamera;
         }
 
         private void OnDisable() {
-            input.OnMouseWheelInput -= ZoomCamera;
+            if (cameraRig)
+                input.OnMouseWheelInput -= ZoomCamera;
         }
 
         private void Start() {
-            CameraSetup();
-
             Cursor.lockState = cursorLockOnStart
                     ? CursorLockMode.Locked
                     : CursorLockMode.None;
         }
 
-        private void LateUpdate() {
+        private void Update() {
             RotateCamera(input.LookDirection.x, -input.LookDirection.y);
         }
 
@@ -65,20 +75,32 @@ namespace SpellBound.Controller.Configuration {
         public Vector3 GetFacingDirection() => _tr.forward;
         
         /// <summary>
-        /// Rotates the camera based on device horizontal and vertical input.
+        /// Rotates the camera based on device horizontal and vertical input about the pivot.
         /// </summary>
         private void RotateCamera(float horizontalInput, float verticalInput) {
+            var targetX = _currentXAngle + verticalInput  * cameraSpeed * Time.deltaTime;
+            var targetY = _currentYAngle + horizontalInput * cameraSpeed * Time.deltaTime;
+
+            targetX = Mathf.Clamp(targetX, -upperVerticalLimit, lowerVerticalLimit);
+
             if (smoothCameraRotation) {
-                horizontalInput = Mathf.Lerp(0, horizontalInput, Time.deltaTime * cameraSmoothingFactor);
-                verticalInput = Mathf.Lerp(0, verticalInput, Time.deltaTime * cameraSmoothingFactor);
+                _currentXAngle = Mathf.LerpAngle(_currentXAngle, targetX, cameraSmoothingFactor * Time.deltaTime);
+                _currentYAngle = Mathf.LerpAngle(_currentYAngle, targetY, cameraSmoothingFactor * Time.deltaTime);
+            } else {
+                _currentXAngle = targetX;
+                _currentYAngle = targetY;
             }
 
-            _currentXAngle += verticalInput * cameraSpeed * Time.deltaTime;
-            _currentYAngle += horizontalInput * cameraSpeed * Time.deltaTime;
+            //var yaw   = Quaternion.AngleAxis(_currentYAngle, Vector3.up);
+            //var pitch = Quaternion.AngleAxis(_currentXAngle, Vector3.right);
+            //cameraPivot.localRotation = Quaternion.AngleAxis(_currentXAngle, yaw * Vector3.right) * yaw;
+
+            //_currentXAngle += verticalInput * cameraSpeed * Time.deltaTime;
+            //_currentYAngle += horizontalInput * cameraSpeed * Time.deltaTime;
 
             _currentXAngle = Mathf.Clamp(_currentXAngle, -upperVerticalLimit, lowerVerticalLimit);
-            
-            cameraPivot.localRotation = Quaternion.Euler(_currentXAngle, _currentYAngle, 0);
+            cameraPivot.localRotation = Quaternion.Euler(_currentXAngle, _currentYAngle, 0f);
+            //cameraPivot.localRotation = Quaternion.Euler(_currentXAngle, _currentYAngle, 0);
         }
 
         private void ZoomCamera(Vector2 zoomInput) {
@@ -96,6 +118,8 @@ namespace SpellBound.Controller.Configuration {
         /// Sets our cameraRig tracking target.
         /// </summary>
         private void CameraSetup() {
+            _brain.WorldUpOverride = cameraPivot;
+            
             cameraRig = _brain.ActiveVirtualCamera as CameraRigManager;
 
             if (cameraRig == null) {
