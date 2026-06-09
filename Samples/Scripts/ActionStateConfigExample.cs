089db772958937c3973ac82678aaba313403a8c7
Judsin <judsin1349@gmail.com>
2026-06-09 15:00:46 -0500

    lil formatting

--- Samples/Scripts/ActionStateConfigExample.cs
+++ Samples/Scripts/ActionStateConfigExample.cs
@@ -1,21 +1,9 @@
 // Copyright 2026 Spellbound Studio Inc.
 
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Spellbound.Controller.Samples {
-    /// <summary>
-    /// One action slot-to-state binding for the sample config.
-    /// </summary>
-    [Serializable]
-    public sealed class ActionStateBindingExample : IStateBinding<ActionStateTypes> {
-        [SerializeField] private ActionStateTypes slot;
-        [SerializeField] private BaseSoState variant;
-        public ActionStateTypes Slot => slot;
-        public BaseSoState Variant => variant;
-    }
-
     /// <summary>
     /// Sample reusable config for the action state machine.
     /// </summary>
--- Samples/Scripts/LocoStateConfigExample.cs
+++ Samples/Scripts/LocoStateConfigExample.cs
@@ -1,21 +1,9 @@
 // Copyright 2026 Spellbound Studio Inc.
 
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Spellbound.Controller.Samples {
-    /// <summary>
-    /// One locomotion slot-to-state binding for the sample config.
-    /// </summary>
-    [Serializable]
-    public sealed class LocoStateBindingExample : IStateBinding<LocoStateTypes> {
-        [SerializeField] private LocoStateTypes slot;
-        [SerializeField] private BaseSoState variant;
-        public LocoStateTypes Slot => slot;
-        public BaseSoState Variant => variant;
-    }
-
     /// <summary>
     /// Sample reusable config for the locomotion state machine.
     /// </summary>
